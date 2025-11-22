b34ce27df2256078f8de6cfbc9d8afc8085807fc
sondt <sondt1301@gmail.com>
2025-11-22 22:49:26 +0700

    Add ansbile and WordCount, and update firewall

--- terraform/firewall.tf
+++ terraform/firewall.tf
@@ -9,3 +9,24 @@ resource "google_compute_firewall" "ssl_allowed" {
 
   source_ranges = ["0.0.0.0/0"]
 }
+
+resource "google_compute_firewall" "internal_allow_all" {
+  name    = "internal-allow-all"
+  network = google_compute_network.vpc_network.name
+
+  allow {
+    protocol = "tcp"
+    ports    = ["0-65535"]
+  }
+
+  allow {
+    protocol = "udp"
+    ports    = ["0-65535"]
+  }
+
+  allow {
+    protocol = "icmp"
+  }
+
+  source_ranges = ["10.0.0.0/16"]
+}
